ebdc6cc02e544adeeede57c9a03de384ecd4a510
Mohd-Saad30 <saadmohd835@gmail.com>
2025-08-30 15:11:26 +0530

    Make Improvement in login page

--- src/components/Auth/LoginPage.jsx
+++ src/components/Auth/LoginPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import {Store } from "../../Context/Context";
 
-export const LoginPage = ({ handleLogin }) => {
+export const LoginPage = () => {
   const [email, setEmail] = useState("");
 
   const [password, setPassword] = useState("");
@@ -18,14 +18,14 @@ export const LoginPage = ({ handleLogin }) => {
     setPassword("");
   };
   return (
-    <div className="flex  flex-col items-center  h-screen bg-blue-100 ">
+    <div className="flex  flex-col items-center min-h-screen bg-blue-100 ">
       <header className="w-full flex items-center bg-gradient-to-r from-blue-500 to-blue-700 p-6  rounded-b-xl justify-center">
-        <h1 className="text-white text-3xl font-bold">
+        <h1 className="text-white text-2xl md:text-3xl font-bold">
           Employee Management System
         </h1>
       </header>
 
-      <div className="mt-auto border-2 rounded-xl border-emerald-600 transition duration-150 p-20 bg-white">
+      <div className="mt-auto w-full max-w-sm border-2 rounded-xl border-blue-700 transition duration-150  p-8 md:p-12  bg-white">
         <form
           className="flex items-center justify-center flex-col"
           onSubmit={submitHandler}
@@ -36,7 +36,7 @@ export const LoginPage = ({ handleLogin }) => {
             onChange={(e) => {
               setEmail(e.target.value);
             }}
-            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400"
+            className="w-full outline-none bg-transparent border-2 border-blue-700 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400"
             type="email"
             placeholder="Enter Your Email"
           />
@@ -46,17 +46,17 @@ export const LoginPage = ({ handleLogin }) => {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
+            className="w-full outline-none bg-transparent border-2 border-blue-700 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
             type="password"
             placeholder="Enter Your Password"
           />
-          <button className="mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full">
+          <button className="mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-500 text-lg py-2 px-8 w-full rounded-full">
             Log In
           </button>
         </form>
       </div>
       <footer className="mt-auto   w-full flex items-center bg-gradient-to-r from-blue-500 to-blue-700 p-6  rounded-b-xl justify-center h-auto">
-        <h1 className="text-white text-3xl font-bold">
+        <h1 className="text-white text-2xl  md:text-3xl font-bold">
           Developed by Mohd Saad
         </h1>
       </footer>
